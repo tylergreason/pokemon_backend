b095b3bf2899caa011119007f164c2a2a6c8b6c4
tylergreason <tyler.greason@protonmail.com>
2020-03-20 08:40:23 -0400

    gave user password_digest and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :user_pokemons, dependent: :destroy 
     has_many :pokemons, through: :user_pokemons
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_212035) do
+ActiveRecord::Schema.define(version: 2020_03_20_123738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_212035) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
