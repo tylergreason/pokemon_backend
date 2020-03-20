90b64dd701f4efdd70e01f4f5a99757f07d47770
tylergreason <tyler.greason@protonmail.com>
2020-03-20 08:24:31 -0400

    moving to authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Pokemon.delete_all
 User.delete_all
-# Type.delete_all 
+
 
 # natures 
 natures = PokeApi.get(:nature).results
@@ -8,37 +8,39 @@ natures.each do |nature|
     name = nature.name
     Nature.find_or_create_by(name:name) 
 end
-
-
-
-fetchPokemon = PokeApi.get(pokemon: 'bulbasaur')
-name = fetchPokemon.name 
-number = fetchPokemon.id
-new_pokemon = Pokemon.create(name:name,number:number)
 # types 
 types = PokeApi.get(:type).results
 types.each do |type| 
     name = type.name 
     Type.find_or_create_by(name:name)
 end
-# nature 
-new_pokemon.natures << Nature.all.sample
-# types
-fetchPokemon.types.each do |type| 
-    new_pokemon.types << Type.find_or_create_by(name: type.type.name)
+
+5.times do 
+    fetch_pokemon = PokeApi.get(pokemon: rand(150)+1)
+    name = fetch_pokemon.name 
+    number = fetch_pokemon.id
+    new_pokemon = Pokemon.find_or_create_by(name:name,number:number)
+    # nature 
+    new_pokemon.natures << Nature.all.sample
+    # types
+    fetch_pokemon.types.each do |type| 
+        new_pokemon.types << Type.find_or_create_by(name: type.type.name)
+    end
+    # abilities
+    abilities =  fetch_pokemon.abilities
+    abilities.each do |result| 
+        new_ability = Ability.find_or_create_by(name:result.ability.name)
+        new_pokemon.abilities << new_ability
+    end
+    # moves 
+    moves = fetch_pokemon.moves 
+    moves.each do |result| 
+        new_move = Move.find_or_create_by(name: result.move.name)
+        new_pokemon.moves << new_move
+    end
 end
-# abilities
-abilities =  fetchPokemon.abilities
-abilities.each do |result| 
-    new_ability = Ability.find_or_create_by(name:result.ability.name)
-    new_pokemon.abilities << new_ability
-end
-# moves 
-moves = fetchPokemon.moves 
-moves.each do |result| 
-    new_move = Move.find_or_create_by(name: result.move.name)
-    new_pokemon.moves << new_move
-end
+
+# fetch_pokemon = PokeApi.get(pokemon: 'bulbasaur')
 
 
 
