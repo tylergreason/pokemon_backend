f6457b1f5342dc6ff4d21c53a9671a48ff9a4947
tylergreason <tyler.greason@protonmail.com>
2020-03-23 16:05:13 -0400

    displaying correct pokemon info. Moving to have changes persist on backend

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,7 +5,8 @@ class PokemonsController < ApplicationController
         puts "this is current user:"
         puts current_user
         pokemons = Pokemon.all
-        render :json => pokemons, :status => :ok
+        # render :json => pokemons, :status => :ok
+        render :json => pokemons, :include => [:types,:moves,:abilities,:natures,:active_moves], :status => :ok 
         # byebug
         # render :json => pokemons, :status => :ok
     end
